a7e1bdcce5c6d12981c9ab0689f75e047525b404
Remi <remi.forest@akilio.com>
2019-04-20 01:19:00 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+import mapr_manager
+
+setup(name='mapr_manager',
+      version=mapr_manager.__version__,
+      packages=find_packages(),
+      description='MapR Cluster management utilities',
+      url='https://github.com/remiforest/mapr-manager',
+      author='Remi Forest',
+      author_email='remi.forest@akilio.com',
+      license='LICENSE.txt',
+    )
