23286a3c520c641a1dc5af39cbe17bf68c7de907
remiforest <remi.forest@akilio.com>
2019-04-26 09:15:38 +0200

    updated setup for requirements

--- setup.py
+++ setup.py
@@ -4,9 +4,13 @@ from setuptools import setup, find_packages
 
 import mapr_manager
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(name='mapr_manager',
       version=mapr_manager.__version__,
       include_package_data=True,
+      install_requires=requirements,
       packages=find_packages(),
       description='MapR Cluster management utilities',
       url='https://github.com/remiforest/mapr-manager',
